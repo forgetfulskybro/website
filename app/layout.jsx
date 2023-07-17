df27542f0eaee8de51181b4565103cae987533c5
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2023-07-17 01:24:25 -0400

    Analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,24 @@
-import './globals.css'
-import { Fira_Code } from 'next/font/google'
+import "./globals.css";
+import { Fira_Code } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const Fira = Fira_Code({ subsets: ['latin'] })
+const Fira = Fira_Code({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'ForGetFulSkybro',
-  description: 'Portfolio for ForGetFulSkyBro',
-}
+  title: "ForGetFulSkybro",
+  description: "Portfolio for ForGetFulSkyBro",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head><link rel="icon" href="/Me.png" sizes="any" /></head>
-      <body className={Fira.className}>{children}</body>
+      <head>
+        <link rel="icon" href="/Me.png" sizes="any" />
+      </head>
+      <body className={Fira.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
