22cb1e813abe37b7b7aff01cbb6d5ca846d2de85
Dominik K <mezo.dsn@gmail.com>
2023-09-22 20:28:15 +0200

    feat(website): improve codebase
    The following pull request adds:
    
    - Svg for the would you logo
    - Better SEO metadata
    - Sort the projects by dates
    -

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,8 +5,16 @@ import { Analytics } from "@vercel/analytics/react";
 const Fira = Fira_Code({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ForGetFulSkybro",
-  description: "Portfolio for ForGetFulSkyBro",
+  title: "ForGetFulSkybro - Portfolio",
+  description: "Hello, my name is ForGetFulSkyBro or Sky for short. I'm currently 19 years old and I've been coding for 4 years. I enjoy creating open source projects on my free time or whenever I'm not lazy.",
+  image: "https://website-forgetfulskybro.vercel.app/Me.png",	
+  url: "https://website-forgetfulskybro.vercel.app",
+  type: "website",
+  site_name: "ForGetFulSkyBro",
+  locale: "en_US",
+  theme_color: '#4ca6ca',
+
+
 };
 
 export default function RootLayout({ children }) {
