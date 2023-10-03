5de8f123ff5c7a1b76a595927572274b933cefc6
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2023-10-02 23:44:59 -0400

    Update: projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -8,163 +8,172 @@ import { UncontrolledTooltip } from "reactstrap";
 export default function Projects() {
   const array = [
     {
-      "target": "WY",
-      "title": "Would You",
-      "image": "/WouldYou.svg",
-      "tags": [{ "name": "Discord Bot", "color": "#5764F3" }],
-      "desc": "An activity based bot that helps improve overall activity for your server.",
-      "footer": {
-        "start": "Aug 22, 2022",
-        "end": "Present"
+      target: "WY",
+      title: "Would You",
+      image: "/WouldYou.svg",
+      tags: [{ name: "Discord Bot", color: "#5764F3" }],
+      desc: "An activity based bot that helps improve overall activity for your server.",
+      footer: {
+        start: "Aug 22, 2022",
+        end: "Present",
       },
-      "flags": [],
-      "github": "https://github.com/Would-You-Bot",
-      "website": "https://wouldyoubot.gg",
-      "community": "https://wouldyoubot.gg/discord"
+      flags: [],
+      github: "https://github.com/Would-You-Bot",
+      website: "https://wouldyoubot.gg",
+      community: "https://wouldyoubot.gg/discord",
     },
     {
-      "target": "SB",
-      "title": "Support Bot",
-      "image": "/Support.png",
-      "tags": [{ "name": "Discord Bot", "color": "#5764F3" }],
-      "desc": "Custom support bot with modmail, giveaways, suggestions, and more.",
-      "footer": {
-        "start": "Sep 30, 2022",
-        "end": "Present"
+      target: "SB",
+      title: "Support Bot",
+      image: "/Support.png",
+      tags: [{ name: "Discord Bot", color: "#5764F3" }],
+      desc: "Custom support bot with modmail, giveaways, suggestions, and more.",
+      footer: {
+        start: "Sep 30, 2022",
+        end: "Present",
       },
-      "flags": [],
-      "github": "https://github.com/forgetfulskybro/Support-Bot",
-      "community": "https://discord.gg/ty6Rsua"
+      flags: [],
+      github: "https://github.com/forgetfulskybro/Support-Bot",
+      community: "https://discord.gg/ty6Rsua",
     },
     {
-      "target": "F",
-      "title": "Functious",
-      "image": "/Functious.png",
-      "tags": [{ "name": "Revolt Bot", "color": "#FE4654" }],
-      "desc": "Revolt bot that allows for creating giveaways, polls, and reaction roles.",
-      "footer": {
-        "start": "Feb 18, 2023",
-        "end": "Present"
+      target: "F",
+      title: "Functious",
+      image: "/Functious.png",
+      tags: [{ name: "Revolt Bot", color: "#FE4654" }],
+      desc: "Revolt bot that allows for creating giveaways, polls, and reaction roles.",
+      footer: {
+        start: "Feb 18, 2023",
+        end: "Present",
       },
-      "flags": [],
-      "github": "https://github.com/forgetfulskybro/Revolt-Functious",
-      "community": "https://rvlt.gg/functious"
-    },
-    {
-      "target": "RBL",
-      "title": "Revolt Bot List",
-      "image": "/Revoltbots.png",
-      "tags": [
-        { "name": "Revolt Bot", "color": "#FE4654" },
-        { "name": "Website", "color": "#3B3E40" }
+      flags: [
+        { name: "Discontinued", color: "#CC222A" },
       ],
-      "desc": "A place where you can find the best Revolt Bots.",
-      "footer": {
-        "start": "Apr 12, 2023",
-        "end": "Present"
-      },
-      "flags": [],
-      "website": "https://revoltbots.org",
-      "github": "https://github.com/BrydenIsNotSmart/Revolt-Bot-List",
-      "community": "https://revoltbots.org/server"
+      github: "https://github.com/forgetfulskybro/Revolt-Functious",
+      community: "https://rvlt.gg/functious",
     },
     {
-      "target": "RM",
-      "title": "Revolt Modmail",
-      "image": null,
-      "tags": [{ "name": "Revolt Bot", "color": "#FE4654" }],
-      "desc": "The first Modmail bot that is made in Revolt.",
-      "footer": {
-        "start": "Feb 4, 2023",
-        "end": "Present"
-      },
-      "flags": [],
-      "github": "https://github.com/forgetfulskybro/Revolt-Modmail",
-      "community": "https://rvlt.gg/functious"
-    },
-    {
-      "target": "RB",
-      "title": "Revolt Bridge",
-      "image": null,
-      "tags": [{ "name": "Revolt Bot", "color": "#FE4654" }],
-      "desc": "A very simple way to bridge your Revolt & Discord servers together.",
-      "footer": {
-        "start": "Feb 7, 2023",
-        "end": "Present"
-      },
-      "flags": [],
-      "github": "https://github.com/forgetfulskybro/Revolt-Bridge",
-      "community": "https://rvlt.gg/functious"
-    },
-    {
-      "target": "D",
-      "title": "Durchie",
-      "image": "/Durchie.png",
-      "tags": [{ "name": "Discord Bot", "color": "#5764F3" }],
-      "desc": "A multipurpose bot that was made to keep your members entertained.",
-      "footer": {
-        "start": "Jun 1, 2021",
-        "end": "Jun 9,2022"
-      },
-      "flags": [
-        { "name": "Contribution", "color": "#4ca6ca" },
-        { "name": "Discontinued", "color": "#CC222A" }
+      target: "RBL",
+      title: "Revolt Bot List",
+      image: "/Revoltbots.png",
+      tags: [
+        { name: "Revolt Bot", color: "#FE4654" },
+        { name: "Website", color: "#3B3E40" },
       ],
-      "community": "https://discord.gg/utilibots-618115853178634240"
+      desc: "A place where you can find the best Revolt Bots.",
+      footer: {
+        start: "Apr 12, 2023",
+        end: "Present",
+      },
+      flags: [
+        { name: "Contribution", color: "#4ca6ca" },
+        { name: "Discontinued", color: "#CC222A" },
+      ],
+      website: "https://revoltbots.org",
+      github: "https://github.com/BrydenIsNotSmart/Revolt-Bot-List",
+      community: "https://revoltbots.org/server",
     },
     {
-      "target": "AYB",
-      "title": "AYB",
-      "image": "/Ayb.png",
-      "tags": [
-        { "name": "Discord Bot", "color": "#5764F3" },
-        { "name": "Website", "color": "#3B3E40" }
-      ],
-      "desc": "Involves AYB Music bot and AYB API that the website used.",
-      "footer": {
-        "start": "Dec 10, 2020",
-        "end": "Unknown"
+      target: "RM",
+      title: "Revolt Modmail",
+      image: null,
+      tags: [{ name: "Revolt Bot", color: "#FE4654" }],
+      desc: "The first Modmail bot that is made in Revolt.",
+      footer: {
+        start: "Feb 4, 2023",
+        end: "Present",
       },
-      "flags": [
-        { "name": "Contribution", "color": "#4ca6ca" },
-        { "name": "Discontinued", "color": "#CC222A" }
+      flags: [
+        { name: "Discontinued", color: "#CC222A" },
       ],
-      "github": "https://github.com/AYB-Archive",
-      "website": "https://ayblisting.com"
+      github: "https://github.com/forgetfulskybro/Revolt-Modmail",
+      community: "https://rvlt.gg/functious",
     },
     {
-      "target": "L",
-      "title": "Luau.gg",
-      "image": "/Luau.jpg",
-      "tags": [{ "name": "Website", "color": "#3B3E40" }],
-      "desc": "Website made by SamOphis on GitHub.",
-      "footer": {
-        "start": "Apr 1, 2022",
-        "end": "Apr 1, 2022"
+      target: "RB",
+      title: "Revolt Bridge",
+      image: null,
+      tags: [{ name: "Revolt Bot", color: "#FE4654" }],
+      desc: "A very simple way to bridge your Revolt & Discord servers together.",
+      footer: {
+        start: "Feb 7, 2023",
+        end: "Present",
       },
-      "flags": [
-        { "name": "Contribution", "color": "#4ca6ca" },
-        { "name": "Discontinued", "color": "#CC222A" }
+      flags: [
+        { name: "Discontinued", color: "#CC222A" },
       ],
-      "github": "https://github.com/SamOphis"
+      github: "https://github.com/forgetfulskybro/Revolt-Bridge",
+      community: "https://rvlt.gg/functious",
     },
     {
-      "target": "FGF",
-      "title": "ForGetFul",
-      "image": "/Forgetful.png",
-      "tags": [
-        { "name": "Discord Bot", "color": "#5764F3" },
-        { "name": "Website", "color": "#3B3E40" }
-      ],
-      "desc": "A multipurpose bot that suited for many needs.",
-      "footer": {
-        "start": "Jul 03, 2019",
-        "end": "Jun 12, 2023"
+      target: "D",
+      title: "Durchie",
+      image: "/Durchie.png",
+      tags: [{ name: "Discord Bot", color: "#5764F3" }],
+      desc: "A multipurpose bot that was made to keep your members entertained.",
+      footer: {
+        start: "Jun 1, 2021",
+        end: "Jun 9,2022",
       },
-      "flags": [{ "name": "Discontinued", "color": "#CC222A" }],
-      "community": "https://discord.gg/ty6Rsua"
-    }
-  ]
+      flags: [
+        { name: "Contribution", color: "#4ca6ca" },
+        { name: "Discontinued", color: "#CC222A" },
+      ],
+      community: "https://discord.gg/utilibots-618115853178634240",
+    },
+    {
+      target: "AYB",
+      title: "AYB",
+      image: "/Ayb.png",
+      tags: [
+        { name: "Discord Bot", color: "#5764F3" },
+        { name: "Website", color: "#3B3E40" },
+      ],
+      desc: "Involves AYB Music bot and AYB API that the website used.",
+      footer: {
+        start: "Dec 10, 2020",
+        end: "Unknown",
+      },
+      flags: [
+        { name: "Contribution", color: "#4ca6ca" },
+        { name: "Discontinued", color: "#CC222A" },
+      ],
+      github: "https://github.com/AYB-Archive",
+      website: "https://ayblisting.com",
+    },
+    {
+      target: "L",
+      title: "Luau.gg",
+      image: "/Luau.jpg",
+      tags: [{ name: "Website", color: "#3B3E40" }],
+      desc: "Website made by SamOphis on GitHub.",
+      footer: {
+        start: "Apr 1, 2022",
+        end: "Apr 1, 2022",
+      },
+      flags: [
+        { name: "Contribution", color: "#4ca6ca" },
+        { name: "Discontinued", color: "#CC222A" },
+      ],
+      github: "https://github.com/SamOphis",
+    },
+    {
+      target: "FGF",
+      title: "ForGetFul",
+      image: "/Forgetful.png",
+      tags: [
+        { name: "Discord Bot", color: "#5764F3" },
+        { name: "Website", color: "#3B3E40" },
+      ],
+      desc: "A multipurpose bot that suited for many needs.",
+      footer: {
+        start: "Jul 03, 2019",
+        end: "Jun 12, 2023",
+      },
+      flags: [{ name: "Discontinued", color: "#CC222A" }],
+      community: "https://discord.gg/ty6Rsua",
+    },
+  ];
 
   return (
     <main key={usePathname()} className="main">
