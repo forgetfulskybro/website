a1097de43324ab29c5deb438ba5aac310d562a59
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2024-01-25 19:33:49 -0500

    Delete src/app/api/auth/[...nextauth] directory

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-import DiscordProvider from "next-auth/providers/discord";
-
-const providers =
-  DiscordProvider({
-    clientId: process.env.DISCORD_CLIENT_ID,
-    clientSecret: process.env.DISCORD_CLIENT_SECRET
-  })
