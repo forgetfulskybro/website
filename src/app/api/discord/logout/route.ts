21895cc0e5ab3b0eacd07226328da2711e93e0a2
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2025-08-05 13:05:45 -0400

    fix?: logout

--- src/app/api/discord/logout/route.ts
+++ src/app/api/discord/logout/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: Request) {
   const response = NextResponse.redirect(
-    new URL("/projects", process.env.NEXT_PUBLIC_BASE_URL)
+    new URL("/projects", request.url)
   );
   response.cookies.set("discord_access_token", "", { maxAge: 0 });
   return response;
